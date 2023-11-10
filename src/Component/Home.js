122fea1972d109b53c2a953ef0fc2f2421b14910
sweswetha-1 <138018625+sweswetha-1@users.noreply.github.com>
2023-11-10 17:13:25 +0530

    Update Home.js

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
     return (
         <section className="home">
-            <h1>SANMATHI PRIYA K R</h1>
+            <h1>SWETHA V</h1>
             <h1>My Portfolio</h1>
             <p>Full stack developer| Coder</p>
         </section>
